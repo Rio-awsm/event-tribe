2179d720efa738c61fa5a81e88920642a2ca1cf1
Supriyo Saha <mm.saha1982@gmail.com>
2023-12-20 03:01:23 +0530

    clerk-auth done

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,8 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
@@ -16,7 +18,14 @@ const Header = () => {
         </Link>
 
         <div className="flex w-32 justify-end gap-3">
-       
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
         </div>
       </div>
     </header>
