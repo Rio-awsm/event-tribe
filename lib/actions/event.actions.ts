2dbf483f3c2ed41b327be65e0b4d65688437e473
Supriyo Saha <mm.saha1982@gmail.com>
2024-01-07 02:55:56 +0530

    fetch all events

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { CreateEventParams } from "@/types";
+import { CreateEventParams, GetAllEventsParams } from "@/types";
 import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
@@ -9,10 +9,14 @@ import Event from "../database/models/event.model";
 import Category from "../database/models/category.model";
 
 const populateEvent = async (query: any) => {
-  return query 
-  .populate({ path: 'organizer', model: User, select: '_id firstName lastName'})
-  .populate({path: 'category', model: Category, select: '_id name' })
-}
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
@@ -38,7 +42,7 @@ export const getEventById = async (eventId: string) => {
   try {
     await connectToDatabase();
 
-    const event = await populateEvent (Event.findById(eventId));
+    const event = await populateEvent(Event.findById(eventId));
 
     if (!event) throw new Error("Event not found");
 
@@ -47,3 +51,32 @@ export const getEventById = async (eventId: string) => {
     handleError(error);
   }
 };
+
+export async function getAllEvents({
+  query,
+  limit = 6,
+  page,
+  category,
+}: GetAllEventsParams) {
+  try {
+    await connectToDatabase();
+
+    const conditions = {}
+
+    const eventsQuery = Event.find(conditions)
+    .sort({ createdAt: 'desc' })
+      .skip(0)
+      .limit(limit);
+
+      const events = await populateEvent(eventsQuery);
+      const eventsCount = await Event.countDocuments(conditions);
+
+      return {
+        data: JSON.parse(JSON.stringify(events)),
+        totalPages: Math.ceil(eventsCount / limit),
+      }
+
+  } catch (error) {
+    handleError(error);
+  }
+}
