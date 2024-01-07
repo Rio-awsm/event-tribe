1265bd42a052ade6c6d222e6d840c7006440ed82
Supriyo Saha <mm.saha1982@gmail.com>
2024-01-07 20:49:11 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <SpeedInsights />
       <html lang="en">
         <body className={poppins.variable}>{children}</body>
       </html>
